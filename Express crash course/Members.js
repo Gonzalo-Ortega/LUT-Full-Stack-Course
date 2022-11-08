4120121af808ecec4d2c68ed1e5b56e3c725c14e
Gooonzalo <g.ortega.2019@alumnos.urjc.es>
2022-11-08 18:30:24 +0200

    Express JS crash course proyect. - Basic server made with Express JS. - Handlebars implementation.

---
+++ Express crash course/Members.js
@@ -0,0 +1,18 @@
+const members = [{
+  id: 1,
+  name: 'Paco',
+  email: 'pago@gmail.com',
+  status: 'active'
+}, {
+  id: 2,
+  name: 'Rober',
+  email: 'rober@gmail.com',
+  status: 'inactive'
+}, {
+  id: 3,
+  name: 'Pepa',
+  email: 'pepa@gmail.com',
+  status: 'active'
+}];
+
+module.exports = members;
