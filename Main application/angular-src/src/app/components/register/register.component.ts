b1c883579a2fa57b177be1889ee2c60dbc05fab8
Gooonzalo <g.ortega.2019@alumnos.urjc.es>
2022-11-15 17:42:03 +0200

    7th tutorial completed. - Added authentification service.

--- Main application/angular-src/src/app/components/register/register.component.ts
+++ Main application/angular-src/src/app/components/register/register.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ValidateService } from '../../services/validate.service';
 import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router';
+import { ValidateService } from '../../services/validate.service';
+import { AuthService } from '../../services/auth.service';
+
 
 @Component({
   selector: 'app-register',
@@ -15,28 +18,42 @@ export class RegisterComponent implements OnInit {
 
   constructor(
     private validateService: ValidateService,
+    private authService: AuthService,
+    private router: Router,
     private flashMessage: FlashMessagesService) { }
 
   ngOnInit() {
   }
 
   onRegisterSubmit() {
-    const user = {
-      name: this.name,
-      email: this.email,
-      username: this.username,
-      password: this.password
-    }
-    // Required Fields
-    if(!this.validateService.validateRegister(user)) {
-      this.flashMessage.show('Please fill in all fields', {cssClass: 'alert-danger', timeout: 3000});
-      return false;
-    }
+      const user = {
+        name: this.name,
+        email: this.email,
+        username: this.username,
+        password: this.password
+      }
 
-    // Validate Email
-   if(!this.validateService.validateEmail(user.email)) {
-   this.flashMessage.show('Please use a valid email', {cssClass: 'alert-danger', timeout: 3000});
-     return false;
-   }
-  }
+      // Required Fields
+      if(!this.validateService.validateRegister(user)) {
+        this.flashMessage.show('Please fill in all fields', {cssClass: 'alert-danger', timeout: 3000});
+        return false;
+      }
+
+      // Validate Email
+      if(!this.validateService.validateEmail(user.email)) {
+        this.flashMessage.show('Please use a valid email', {cssClass: 'alert-danger', timeout: 3000});
+          return false;
+      }
+
+      // Register user
+      this.authService.registerUser(user).subscribe(data => {
+        if(data.success) {
+          this.flashMessage.show('You are now registered and can now login', {cssClass: 'alert-success', timeout: 3000});
+          this.router.navigate(['/login']);
+        } else {
+          this.flashMessage.show('Something went wrong', {cssClass: 'alert-danger', timeout: 3000});
+          this.router.navigate(['/register']);
+        }
+      });
+    }
 }
